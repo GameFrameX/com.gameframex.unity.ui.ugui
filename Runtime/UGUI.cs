ec6a51d78c26549733d1e6a0b5e3d5e6ade89fed
Blank <wangfj11@foxmail.com>
2024-08-30 11:23:50 +0800

    [修改]1. 修改界面基类

--- Runtime/UGUI.cs
+++ Runtime/UGUI.cs
@@ -6,43 +6,38 @@ namespace GameFrameX.UI.UGUI.Runtime
     public class UGUI : UIFormLogic
     {
         /// <summary>
-        /// UI 对象
-        /// </summary>
-        public GameObject GObject { get; }
-
-        /// <summary>
         /// 设置UI的显示状态，不发出事件
         /// </summary>
         /// <param name="value"></param>
         protected override void InternalSetVisible(bool value)
         {
-            if (GObject.activeSelf == value)
+            if (gameObject.activeSelf == value)
             {
                 return;
             }
 
-            GObject.SetActive(value);
+            gameObject.SetActive(value);
         }
 
         public override bool Visible
         {
             get
             {
-                if (GObject == null)
+                if (gameObject == null)
                 {
                     return false;
                 }
 
-                return GObject.activeSelf;
+                return gameObject.activeSelf;
             }
             set
             {
-                if (GObject == null)
+                if (gameObject == null)
                 {
                     return;
                 }
 
-                if (GObject.activeSelf == value)
+                if (gameObject.activeSelf == value)
                 {
                     return;
                 }
@@ -59,7 +54,7 @@ namespace GameFrameX.UI.UGUI.Runtime
                     // OnHideAfterAction?.Invoke(this);
                 }
 
-                GObject.SetActive(value);
+                gameObject.SetActive(value);
                 if (value)
                 {
                     // OnShowBeforeAction?.Invoke(this);
@@ -76,105 +71,11 @@ namespace GameFrameX.UI.UGUI.Runtime
         }
 
         /// <summary>
-        /// 销毁UI对象
-        /// </summary>
-        public override void Dispose()
-        {
-            base.Dispose();
-            // if (IsDisposed)
-            // {
-            //     return;
-            // }
-            //
-            // IsDisposed = true;
-            // // 删除所有的孩子
-            // DisposeChildren();
-            //
-            // // 删除自己的UI
-            // if (!IsRoot)
-            // {
-            //     RemoveFromParent();
-            // }
-            //
-            // // 释放UI
-            // OnDispose();
-            // // 删除自己的UI
-            // if (!IsRoot)
-            // {
-            //     GObject.Dispose();
-            // }
-            //
-            // Release(false);
-            // isFromFGUIPool = false;
-        }
-
-        /// <summary>
-        /// 添加UI对象到子级列表
-        /// </summary>
-        /// <param name="ui"></param>
-        /// <param name="index">添加到的目标UI层级索引位置</param>
-        /// <exception cref="Exception"></exception>
-        // protected override void AddInner(UI.Runtime.UI ui, int index = -1)
-        // {
-        //     var fui = (FUI)ui;
-        //     Children.Add(ui.Name, ui);
-        //     if (index < 0 || index > Children.Count)
-        //     {
-        //         GObject.asCom.AddChild(fui.GObject);
-        //     }
-        //     else
-        //     {
-        //         GObject.asCom.AddChildAt(fui.GObject, index);
-        //     }
-        //
-        //     fui.Parent = this;
-        //
-        //     if (fui.IsInitVisible)
-        //     {
-        //         // 显示UI
-        //         fui.Show(fui.UserData);
-        //     }
-        // }
-
-        /// <summary>
         /// 设置当前UI对象为全屏
         /// </summary>
         protected override void MakeFullScreen()
         {
-            GObject?.GetOrAddComponent<RectTransform>()?.MakeFullScreen();
-        }
-
-        /// <summary>
-        /// 删除指定UI名称的UI对象
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        /*public override bool Remove(string name)
-        {
-            if (Children.TryGetValue(name, out var ui))
-            {
-                Children.Remove(name);
-
-                if (ui != null)
-                {
-                    ui.RemoveChildren();
-
-                    ui.Hide();
-
-                    if (IsComponent)
-                    {
-                        GObject.asCom.RemoveChild(((FUI)ui).GObject);
-                    }
-
-                    return true;
-                }
-            }
-
-            return false;
-        }*/
-        public UGUI(GameObject gObject, object userData = null, bool isRoot = false)
-        {
-            GObject = gObject;
+            gameObject?.GetOrAddComponent<RectTransform>()?.MakeFullScreen();
         }
     }
 }
