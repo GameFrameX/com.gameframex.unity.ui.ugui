3eb6ac8ec81e60b68437025350f6ead783245c3c
Blank <wangfj11@foxmail.com>
2024-11-15 17:07:03 +0800

    [增加]1. 增加按钮事件的监听和注销

---
+++ Runtime/UGUIButtonExtension.cs
@@ -0,0 +1,54 @@
+﻿using GameFrameX.Runtime;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace GameFrameX.UI.UGUI.Runtime
+{
+    /// <summary>
+    /// UGUI按钮扩展
+    /// </summary>
+    public static class UGUIButtonExtension
+    {
+        /// <summary>
+        /// 添加按钮点击事件
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="action">点击回调</param>
+        public static void Add(this Button.ButtonClickedEvent self, UnityAction action)
+        {
+            GameFrameworkGuard.NotNull(action, nameof(action));
+            self.AddListener(action);
+        }
+
+        /// <summary>
+        /// 移除按钮点击事件
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="action">点击回调</param>
+        public static void Remove(this Button.ButtonClickedEvent self, UnityAction action)
+        {
+            GameFrameworkGuard.NotNull(action, nameof(action));
+            self.RemoveListener(action);
+        }
+
+        /// <summary>
+        /// 清除按钮点击事件
+        /// </summary>
+        /// <param name="self"></param>
+        public static void Clear(this Button.ButtonClickedEvent self)
+        {
+            self.RemoveAllListeners();
+        }
+
+        /// <summary>
+        /// 设置按钮点击事件
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="action">点击回调</param>
+        public static void Set(this Button.ButtonClickedEvent self, UnityAction action)
+        {
+            GameFrameworkGuard.NotNull(action, nameof(action));
+            self.Add(action);
+        }
+    }
+}
