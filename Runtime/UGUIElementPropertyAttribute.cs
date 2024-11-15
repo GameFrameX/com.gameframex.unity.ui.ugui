4083203f02de1b859d258eb01ee8ebe4679d1d8f
Blank <wangfj11@foxmail.com>
2024-11-15 16:46:52 +0800

    [增加]1. 增加节点的路径标记属性

---
+++ Runtime/UGUIElementPropertyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GameFrameX.UI.UGUI.Runtime
+{
+    /// <summary>
+    /// UGUI控件属性
+    /// </summary>
+    public sealed class UGUIElementPropertyAttribute : Attribute
+    {
+        /// <summary>
+        /// 控件路径
+        /// </summary>
+        public string Path { get; }
+
+        public UGUIElementPropertyAttribute(string path)
+        {
+            Path = path;
+        }
+    }
+}
