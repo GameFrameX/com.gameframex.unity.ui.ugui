70b064ab01153af3f60d8c95f058f003e6fbecfc
Blank <wangfj11@foxmail.com>
2024-11-30 17:30:02 +0800

    [增加]1. 增加图片的加载

---
+++ Runtime/UGUIImageExtension.cs
@@ -0,0 +1,28 @@
+﻿using GameFrameX.Asset.Runtime;
+using GameFrameX.Runtime;
+using UnityEngine;
+
+namespace GameFrameX.UI.UGUI.Runtime
+{
+    /// <summary>
+    /// UGUI图片扩展
+    /// </summary>
+    public static class UGUIImageExtension
+    {
+        /// <summary>
+        /// 设置Icon
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="icon">icon地址</param>
+        public static async void SetIcon(this UnityEngine.UI.Image self, string icon)
+        {
+            var assetComponent = GameEntry.GetComponent<AssetComponent>();
+            var valueHandle = await assetComponent.LoadAssetAsync<Texture2D>(icon);
+            if (valueHandle.IsSucceed)
+            {
+                var texture2D = valueHandle.GetAssetObject<Texture2D>();
+                self.sprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), new Vector2(0.5f, 0.5f));
+            }
+        }
+    }
+}
---
+++ Runtime/UIImage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace GameFrameX.UI.UGUI.Runtime
+{
+    [DisallowMultipleComponent]
+    public class UIImage : UnityEngine.UI.Image
+    {
+        private string m_icon { get; set; }
+
+        /// <summary>
+        /// Icon
+        /// </summary>
+        public string icon
+        {
+            get { return m_icon; }
+            set
+            {
+                if (m_icon != value)
+                {
+                    m_icon = value;
+                    this.SetIcon(m_icon);
+                }
+            }
+        }
+    }
+}
